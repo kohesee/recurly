47e82a142cf63dcc7ca0a8cd307dddb6504d9b5b
Kaivalya <kaivalya.beohar@gmail.com>
2026-06-20 16:47:33 +0530

    learnt navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,5 @@
-import "@/global.css"
-import { Text, View } from "react-native";
- 
+import { Redirect } from "expo-router";
+
 export default function App() {
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
-  );
+  return <Redirect href="/(tabs)" />;
 }
