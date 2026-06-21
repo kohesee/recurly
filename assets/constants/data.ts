a9148e9120ee339020524bfccc46b0a77b8198ed
Kaivalya <kaivalya.beohar@gmail.com>
2026-06-21 13:24:48 +0530

    feat: setup, routing, navbar

---
+++ assets/constants/data.ts
@@ -0,0 +1,9 @@
+import {icons} from './icons';
+
+export const tabs = [
+    { name: 'index',title: 'Home', icon: icons.home },
+    { name: 'subscriptions',title: 'Subscriptions', icon: icons.wallet },
+    { name: 'insights',title: 'Inights', icon: icons.activity },
+    { name: 'settings',title: 'Settings', icon: icons.setting },
+
+];
